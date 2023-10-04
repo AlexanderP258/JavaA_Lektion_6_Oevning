eb8e05c9fe1d5258c72404dc89d35e406f3704f6
Alexander Pumarin <alexander@pumarin.se>
2023-10-04 09:09:40 +0200

    Breakpoint

--- src/com/alexander/Main.java
+++ src/com/alexander/Main.java
@@ -2,6 +2,15 @@ package com.alexander;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Application has started");
 
+        // Create for loop
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Iteration " + i);
+            // Create a breakpoint here
+            // Breakpoint will pause runtime at this point
+        }
+
+        System.out.println("Application has ended");
     }
 }
